feee56f39623244be346ef4e0d2174e3e9ade9f5
Hao Zhang <klaushaozhang.note@gmail.com>
2015-10-30 11:03:58 +1300

    multiple samples

--- learn_ninject_xml/Program.cs
+++ learn_ninject_xml/Program.cs
@@ -9,6 +9,10 @@ namespace learn_ninject_xml
     {
       var kernel = new StandardKernel();
       kernel.Load("encryptorModule.xml");
+      // Load multiple xml files
+      // kernel.Load("module1.xml","module2.xml","module3.xml");
+      // kernel.Load("*.xml");
+      // kernel.load(""Modules/*.xml");
       var encryptor = kernel.Get<IEncryptor>();
       Console.WriteLine(encryptor.Encrypt("Hello"));
       Console.ReadKey();
